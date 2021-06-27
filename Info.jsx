9365af88a3ceb8a287cd1b561c43f41f15f87bc2
Giftlin29 <84953376+Giftlin29@users.noreply.github.com>
2021-06-27 13:35:53 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>React js and javascript</h1>
+      <p>
+        This bootcamp gives you a basic knowledge about javascript and React js
+      </p>
+    </div>
+  );
+}
+export default Info;
